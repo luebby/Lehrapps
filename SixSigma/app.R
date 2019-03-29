159211d9b03592f30b131bf95a656b655c35e933
Bia-Ka <bianca.krol@fom.de>
2019-03-29 10:37:17 +0100

    nur ein paar Kommata

--- SixSigma/app.R
+++ SixSigma/app.R
@@ -18,20 +18,20 @@ tabPanel("Hintergrund",
          fluidPage(
            titlePanel("FOMshiny: Six Sigma"),
            fluidRow(column(12, h3("Hintergrund"))),
-           fluidRow(column(12, "Überall gibt es Variation, z.B. auch bei der Herstellung von Produkten.")),
-           fluidRow(column(12, "Wird ein Produkt innerhalb der zulässigen Toleranzen produziert, wird es akzeptiert, andernfalls muss es nachgearbeitet oder weggeworfen werden.")),
-           fluidRow(column(12, "Wie oft ein Produkt außerhalb der Toleranzen liegt hängt von der Standardabweichung ab.")),
-           fluidRow(column(12, "Ist diese klein, wird wenig Ausschuss produziert, ist diese groß wird viel Ausschuss produziert.")),
-           fluidRow(column(12, "Dafür kann man die sogenannten sigma-Bereiche angeben: wie viele Standardabweichungen nach oben oder unten von der Sollgröße (Mittelwert) können (noch) akzeptiert werden?")),
+           fluidRow(column(12, "Überall gibt es Variation, z. B. auch bei der Herstellung von Produkten.")),
+           fluidRow(column(12, "Wird ein Produkt innerhalb der zulässigen Toleranzen produziert, wird es akzeptiert, andernfalls muss es nachgearbeitet oder als Ausschuss entsorgt werden.")),
+           fluidRow(column(12, "Wie viele Produkte außerhalb der Toleranzen liegen, hängt von der Standardabweichung ab.")),
+           fluidRow(column(12, "Ist diese klein, wird wenig Ausschuss produziert, ist diese groß, wird viel Ausschuss produziert.")),
+           fluidRow(column(12, "Dafür kann man die sogenannten Sigma-Bereiche angeben: Wie viele Standardabweichungen nach oben oder unten von der Sollgröße (Mittelwert) können (noch) akzeptiert werden?")),
            fluidRow(column(12, h4("Prozessqualität"))),
-           fluidRow(column(12, "Angenommen die Produktionsschwankungen seien normalverteilt.")),
-           fluidRow(column(12, "Die Solllänge eines Stahlrohres sei 100mm, der Toleranzbereich gehe von 99.95mm bis 100.05mm.")),
+           fluidRow(column(12, "Angenommen, die Produktionsschwankungen seien normalverteilt.")),
+           fluidRow(column(12, "Die Solllänge eines Stahlrohres sei 100 mm, der Toleranzbereich gehe von 99.95 mm bis 100.05 mm.")),
            fluidRow(column(12, "Wie viel Ausschuss wird in Abhängigkeit der Prozessqualität produziert?"))
       )),
                  
 tabPanel("Prozessqualität", 
          fluidPage(
-           fluidRow(sliderInput("sigma", "Prozessqualität sigma", value = 3, min=0.5, max=6, step=0.5)),
+           fluidRow(sliderInput("sigma", "Prozessqualität Sigma", value = 3, min=0.5, max=6, step=0.5)),
            fluidRow(plotOutput("Plotpnorm"))
            )
          )
@@ -62,13 +62,13 @@ server <- function(input, output) {
             fill = ~ (abs(x-100) <= 0.05), geom = "area") %>%
       gf_lims(x=c(100-3*0.05, 100+3*0.05), 
               y=c(0, 50)) %>%
-      gf_refine(guides(fill=guide_legend(title="Akzeptiert"))) %>%
+      gf_refine(guides(fill=guide_legend(title="akzeptiert"))) %>%
       gf_vline(xintercept = sigmabereiche, color = "lightgray") %>%
       gf_vline(xintercept = toleranz, color = "red") %>%
       gf_vline(xintercept = 100) %>%
       gf_labs(title = bquote(Prozess~funktioniert~zum~Niveau~.(sigma)~sigma), 
-              subtitle = paste("Erwarterer Ausschuss bei 1.000.000 Teile: ", ausschuss),
-              caption = "ohne Berücksichtigung einer evt. Mittelwertsverschiebung") %>%
+              subtitle = paste("Erwarterer Ausschuss bei 1.000.000 Teilen: ", ausschuss),
+              caption = "ohne Berücksichtigung einer evtl. Mittelwertsverschiebung") %>%
       gf_theme(theme_classic(22*1.01)) %>%
       gf_theme(legend.position = "bottom")
     })
