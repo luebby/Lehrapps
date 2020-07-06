9e29f40d0bd03833d726d48248f1c0c6fd57ba33
Norman Markgraf <nmarkgraf@hotmail.com>
2020-07-06 14:01:36 +0200

    CheckVorhersageCSV
    Prüft ob eine CSV Datei 100 Beobachtungen mit zwei Variabeln hat ;-)

---
+++ CheckVorhersageCSV/app.R
@@ -0,0 +1,113 @@
+# ===========================================================================
+# app.R (Release 0.1)
+# =====----------------------------------------------------------------------
+#
+# Check Vorhersagen CDV
+# ---------------------
+#
+# (W) by Norman Markgraf in 2020
+#
+# 06. Jul. 2020  (nm)  Aller erste Version (0.1)
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+#
+#   (C)opyleft Norman Markgraf (nmarkgraf@hotmail.com) in 2020
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#  Einstein’s Dictum:
+#
+#     “Everything should be as simple as possible, but no simpler.”
+#
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+library(shiny)
+
+options(shiny.maxRequestSize = 100*1024^2)
+DEBUG <- FALSE
+
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+    titlePanel("Test"),
+    
+    sidebarLayout(
+        sidebarPanel(
+            navbarPage("Input", id = "allResults",
+                       shiny::tabPanel(
+                           value = 'inputData', 
+                           title = 'Data Import',
+                           br(),
+                           
+                           h4("Import data"),
+                           
+                           shiny::fileInput(inputId = "inFile", "Choose a CSV File",
+                                            accept = c(
+                                                "text/csv",
+                                                "text/comma-separated-values,text/plain",
+                                                ".csv"
+                                            )
+                           ),
+                           
+                           checkboxInput("header", "Header", TRUE)
+                       )
+            )
+        ),
+        mainPanel(
+            # tableOutput("dispTable")
+            htmlOutput("checkTable")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+    
+    output$checkTable <- renderText({
+        inFile <- input$inFile
+        if (is.null(inFile)) {
+            return("<p class=\"error\">Noch keine Datei hochgeladen!<p>")
+        }
+        df <- read.csv2(inFile$datapath, header = input$header)
+        ausgabe <- ""
+        x <- 0
+        if (nrow(df) != 100) {
+            ausgabe <- "<p class=\"error\">Die Antwort muss 81 Zeilen enthalten!</p>"
+            x <- 1
+        }
+        if (ncol(df) != 2) {
+            ausgabe <- paste(ausgabe, 
+                             "<p class=\"error\">Die Antwort muss 2 Spalten enthalten!</p>", 
+                             sep = "\r\n")
+            x <- x + 2
+        }
+        if (x == 2) {
+            ausgabe <- "<p class=\"okay\">Alles okay!</p>"
+        }
+        if (DEBUG) {
+            paste(ausgabe, nrow(df), ncol(df), sep = "\r\n")
+        } else {
+            ausgabe
+        }
+    }
+    )
+    
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
