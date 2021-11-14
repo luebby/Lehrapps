87379b7014e85e07a74b7c538c5887f1731dc816
NMarkgraf <nmarkgraf@hotmail.com>
2021-11-14 17:20:13 +0100

    Kleine Ändereungen in Richtung ggformula. Etwas "entkernt", da einige Dinge (vermutlich) unnötig sind. HTML etwas angepasst.
    *** BITTE TESTEN ***

--- Regressioncontest/app.R
+++ Regressioncontest/app.R
@@ -10,6 +10,7 @@
 #
 
 library(shiny)
+library(mosaic)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -18,11 +19,11 @@ ui <- fluidPage(
   
   # Sidebar with a slider input for number of bins 
   h3("Ihr Auftrag:"),
-  h5("Mit Hilfe Ihrer menschlichen Intelligenz: Finden Sie den Zusammenhang zwischen x und y, 
+  p("Mit Hilfe Ihrer menschlichen Intelligenz: Finden Sie den Zusammenhang zwischen x und y, 
      zwischen der Anzahl Kontakte bei Facebook und Instagram!"),
-  h5("Tun Sie dies indem Sie zwei Punkte auf der Abbildung klicken und diese werden mit einer Gerade verbunden. 
+  p("Tun Sie dies indem Sie zwei Punkte auf der Abbildung klicken und diese werden mit einer Gerade verbunden. 
      Eine neue Linie entsteht wenn Sie noch einmal klicken. Je niedriger Ihr Score desto besser!"),
-  h5("Sind Sie besser als die Künstliche Intelligenz von Dr. Stat?"),
+  p(strong("Sind Sie besser als die Künstliche Intelligenz von Dr. Stat?")),
   # 
   # h5("IF YOU WANT TO LOAD YOUR DATA..."),
   # h6("Check the box if your data file has a header. Choose a CSV File with at least two numerical columns and then click on the plot. 
@@ -35,7 +36,7 @@ ui <- fluidPage(
   plotOutput("plot1", click = "plot_click"),
   textOutput("check"), # not useful
   hr(),
-  h5("Zufrieden? Dann schreiben Sie Ihren Score in den Chat!"),
+  p("Zufrieden? Dann schreiben Sie Ihren Score in den Chat!"),
   # shiny::actionButton(inputId='ab1', label="Submit!", 
   #                     icon = icon("th"), 
   #                     onclick ="window.open('https://jacopodiiorio.typeform.com/to/PU9zHY', '_blank')")
@@ -46,72 +47,98 @@ ui <- fluidPage(
 server <- function(input, output) {
   
   # available data
-  mydata <- read.table(file="data/socialdata.csv", sep=',', header=TRUE)
-  colnames(mydata) <- c('x','y') 
+  read.csv("data/socialdata.csv",
+           colClasses = c("numeric", "numeric"),
+           col.names = c("x", "y")
+           ) %>% mutate( type = "data") -> mydata
+  #colnames(mydata) <- c('x','y', "type")
   click_saved <- reactiveValues(singleclick = NULL) #niente cliccato
   
   # plot is available
   output$plot1 <- renderPlot({
-    if(!is.null(input$file1)){
-      mydata <- read.table(input$file1$datapath,
-                           header = input$header,
-                           row.names = NULL,
-                           sep = ',')
-      mydata <- mydata %>% dplyr::select(where(is.numeric))
-      mydata <- mydata[,c(1,2)]
-      colnames(mydata) <- c('x','y')
-    }
-    prova <- dim(mydata)[1]
-    plot(mydata$x, mydata$y, xlab='x', ylab='y', pch=16, asp=1)
+    # plot(mydata$x, mydata$y, xlab = 'x', ylab = 'y', pch = 16, asp = 1)
+    gf_point(y ~ x, data = filter(mydata, type == "data")) %>%
+      gf_theme(theme_classic())
   })
   
   # if you click
   observeEvent(eventExpr = input$plot_click, handlerExpr = { 
 
     click_saved$singleclick <- rbind(click_saved$singleclick, c(input$plot_click[1], input$plot_click[2])) 
-    if(dim(click_saved$singleclick)[1]>2)
+    if (dim(click_saved$singleclick)[1] > 2) {
       click_saved$singleclick <- as.data.frame(click_saved$singleclick[dim(click_saved$singleclick)[1],])
-      
-    if(!is.null(input$file1)){
-      mydata <- read.table(input$file1$datapath,
-                           header = input$header,
-                           row.names = NULL,
-                           sep = ',')
-      mydata <- mydata %>% dplyr::select(where(is.numeric))
-      mydata <- mydata[,c(1,2)]
-      colnames(mydata) <- c('x','y')
     }
     
     output$plot1 <- renderPlot({
-      plot(mydata$x, mydata$y, xlab='x', ylab='y', pch=16, asp=1)
+      ys <- as.vector(unlist(click_saved$singleclick[,2]))
+      xs <- as.vector(unlist(click_saved$singleclick[,1]))
+      clicked_points <- data.frame( 
+        x = xs, 
+        y = ys) %>% mutate(type = "clicked")
       
-      points(click_saved$singleclick[,1], click_saved$singleclick[,2], col='red', pch=16)
-      if(dim(click_saved$singleclick)[1]==2){
-        ys <- as.vector(unlist(click_saved$singleclick[,2]))
-        xs <- as.vector(unlist(click_saved$singleclick[,1]))
-        fit <- lm(ys~xs)
-        abline(fit, col='red')
-        
-        # projections
-        proj <- fit$coefficients[2] * mydata$x + fit$coefficients[1] 
-        points(mydata$x, proj, col='purple')
-        
-        # residuals
-        myscore <- NULL
-        
-        for(k in 1:dim(mydata)[1]){
-          residuals <- rbind( mydata[k,], c(mydata[k,1], proj[k]) )
-          lines(residuals, col='purple')
-          
-          myscore <- rbind(myscore,(residuals[1,] -residuals[2,])^2)
+      plt <- gf_point(y ~ x, 
+                      color = ~type, 
+                      show.legend = FALSE,
+                      data = rbind(mydata, clicked_points)) 
+      
+      # plot(mydata$x, mydata$y, xlab = 'x', ylab = 'y', pch = 16, asp = 1)
+      # points(click_saved$singleclick[,1], click_saved$singleclick[,2], col = 'red', pch = 16)
+      if (dim(click_saved$singleclick)[1] == 2) {
+        if (xs[1] == xs[2] && ys[1] == ys[2]) {
+          # Falls jemand zu schnell auf einer Stelle klickt gibt es einen Fehler.
+          # Hiermit umgehen wir dieses in dem wir eine echte Steigung erzeugen!
+          xs[2] = xs[2] + 10
+          ys[2] = ys[2] + 10
         }
-        mtext(paste("Ihr Score: ", round(sum(colSums(myscore))/(dim(mydata)[1]),2)), side=3)
+        # Erzeuge ein Gerade durch die zwei geklickten Punkte
+        fit <- lm(ys ~ xs)
         
-        output$check <- renderText(paste("Ihre Gleichung: y = ", round(fit$coefficients[1],2), 
-                                         "+", 
-                                         round(fit$coefficients[2],2),
-                                         "* x"))
+        # Berechne in proj die durch das Modell geschätzen Werte:
+        proj <- fit$coefficients[2] * mydata$x + fit$coefficients[1] 
+        
+        proj_data <- data.frame(
+          x = mydata$x,
+          y = proj
+        ) %>% mutate( type = "residuals" )
+        plt <- gf_point(y ~ x, 
+                        color = ~type, 
+                        show.legend = FALSE,
+                        data = rbind(mydata, clicked_points, proj_data)) 
+        #points(mydata$x, proj, col = 'purple')
+
+        # Zeichne Gerade durch die zwei geklickten Punkte:
+        plt %>% gf_coefline(coef = fit$coefficients, color = "red") -> plt
+        #abline(fit, col = 'red')
+
+                # residuals
+        myscore <- 0
+        
+        for (k in 1:dim(mydata)[1]) {
+          residuals <- rbind( mydata[k,], c(mydata[k,1], proj[k]) )
+          # print(str(residuals))
+          plt %>% gf_line(y ~ x, color = "purple", data = residuals) -> plt
+          # lines(residuals, col = 'purple')
+          
+          myscore <- myscore + (mydata[k,1] - proj[k])^2
+        }
+        
+        plt %>% gf_labs(
+          title = paste("Ihr Score: ", round(myscore/(dim(mydata)[1]),2))
+        ) -> plt
+        output$check <- renderText(
+          paste("Ihre Gleichung: y = ", 
+                round(fit$coefficients[1],2), 
+                "+",
+                round(fit$coefficients[2],2),
+                intToUtf8(183), # UTF-8 int für &centerdot;
+                "x"))
       }
+      our_colors <- c("data" = "black", "clicked" = "red", "residuals" = "purple")
+      plt %>% 
+        gf_refine(
+          scale_color_manual(values = our_colors)
+          ) %>%
+        gf_theme(theme_classic())
     })
   })
 }
