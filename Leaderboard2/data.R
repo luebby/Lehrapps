949cb8ebe461dd497b3ee560049f83a891fe7f72
luebby <karsten@statistix.org>
2022-01-14 12:31:37 +0100

    Schwierigere Daten

--- Leaderboard2/data.R
+++ Leaderboard2/data.R
@@ -4,8 +4,8 @@ set.seed(1896)
 n <- 80
 
 x <- rnorm(n, mean = 8, sd = 5)
-y <- 8000 - 8*x
-y <- y + rnorm(n, sd=sd(y)/2)
+y <- 8000 - 8*x + 0.5*(x-15)^2
+y <- y + rnorm(n, sd=(24+8*(abs(x-10))))
 
 energy <- data.frame(temperature = x, consumption = y)
 write.csv(energy, "Leaderboard2/data/energy.csv", row.names = FALSE)
