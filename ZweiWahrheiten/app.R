1271bc9275a1e3f97c90d0755e5e090d14634997
luebby <karsten@statistix.org>
2023-03-03 10:42:52 +0100

    Uhrzeitabfrage.

--- ZweiWahrheiten/app.R
+++ ZweiWahrheiten/app.R
@@ -1,6 +1,8 @@
 library(googlesheets4)
 library(shiny)
+library(shinyTime)
 library(lubridate)
+library(hms)
 library(dplyr)
 
 gs4_deauth()
@@ -9,12 +11,16 @@ results <- results %>%
   rename(Sicherheit = `Wie sicher sind Sie, die Lüge erkannt zu haben?`,
          Richtig = `Lagen Sie richtig?`) %>%
   mutate(Datum = date(Zeitstempel)) %>%
+  mutate(Zeit = as_hms(Zeitstempel)) %>%
   mutate(Korrekt = ifelse(Richtig == "Ja", 1, 0)) %>%
-  select(Datum, Korrekt)
+  select(Datum, Zeit, Korrekt)
 
 ui <- fluidPage(
     h3("Auswertung Zwei Wahrheiten, eine Lüge"),
+    p("Hinweis: Für Datenaktualisierung App schließen und neu starten."),
     dateInput("date", "Datum:"),
+    timeInput("time1", "Startzeit:", value = Sys.time()-15*60, seconds = FALSE),
+    timeInput("time2", "Endzeit:", value = Sys.time(), seconds = FALSE),
     tableOutput('table')
     )
   
@@ -23,5 +29,6 @@ shinyApp(ui, server = function(input, output) {
   output$table <- renderTable(
     results %>%
       filter(Datum == input$date) %>%
+      filter(Zeit >= as_hms(input$time1) & Zeit <= as_hms(input$time2)) %>%
       summarise(n = n(), richtig=sum(Korrekt)))
   })
