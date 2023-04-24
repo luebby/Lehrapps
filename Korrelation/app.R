7f0f5d212af460feaca33900d08c2d1c977a16ae
NMarkgraf <nmarkgraf@hotmail.com>
2023-04-24 17:10:18 +0200

    Kleinere Anpassung um die Ausführungsgeschwindigkeit zu erhöhen.

--- Korrelation/app.R
+++ Korrelation/app.R
@@ -3,25 +3,29 @@ library(mosaic)
 library(mvtnorm)
 
 ui <- bootstrapPage(
-  sliderInput("r","Korrelationskoeffzient", value = 0, min=-1, max=1, step=0.01),
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "fom.css")
+  ),
+  sliderInput("r", "Korrelationskoeffzient", 
+              value = 0, min = -1, max = 1, step = 0.01),
   plotOutput("Plotcor", height = "500px", width = "500px"))
 
 server <- function(input, output) {
   
   output$Plotcor <- renderPlot({
-    r<- input$r
+    r <- input$r
     sigma <- matrix(c(1,r,r,1), 2)
-    xy <- rmvnorm(96, sigma = sigma)
-    xy <- data.frame(
-      x = xy[,1],
-      y = xy[,2]
-    )
-    
-    options(repr.plot.width = 5, repr.plot.height =5)
-    gf_point(x ~ y, data = xy, color = "darkgreen", size = 2) %>%
-      gf_lims(x = c(-3.5,3.5), y=c(-3.5,3.5)) %>%
-      gf_vline(xintercept = ~ 0, color = "darkgrey") %>%
-      gf_hline(yintercept = ~ 0, color = "darkgrey") %>%
+    rmvnorm(96, sigma = sigma) %>%  # " |> . => " geht leider noch nicht.
+      tibble(
+        x = .[,1],
+        y = .[,2]
+      ) -> xy
+
+    options(repr.plot.width = 5, repr.plot.height = 5)
+    gf_point(x ~ y, data = xy, color = "darkgreen", size = 2) |>
+      gf_lims(x = c(-3.5, 3.5), y = c(-3.5, 3.5)) |>
+      gf_vline(xintercept = ~ 0, color = "darkgrey") |>
+      gf_hline(yintercept = ~ 0, color = "darkgrey") |>
       gf_theme(theme_void())
     })
 }
